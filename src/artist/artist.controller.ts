34abe29f837f97a3e5bcebf30aa9fb52e345946a
Constantine Kolobov <kolobovconstantine@gmail.com>
2025-11-29 18:18:26 +0100

    feat: remove deleted artist id from tracks and albums

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -12,13 +12,21 @@ import {
   BadRequestException,
 } from '@nestjs/common';
 import { validate as isValidUUID } from 'uuid';
-import { ArtistService } from '../database/services';
+import {
+  ArtistService,
+  TrackService,
+  AlbumService,
+} from '../database/services';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
 
 @Controller('artist')
 export class ArtistController {
-  constructor(private readonly artistService: ArtistService) {}
+  constructor(
+    private readonly artistService: ArtistService,
+    private readonly trackService: TrackService,
+    private readonly albumService: AlbumService,
+  ) {}
 
   @Get()
   @HttpCode(HttpStatus.OK)
@@ -80,6 +88,22 @@ export class ArtistController {
     if (!artist) {
       throw new NotFoundException(`Artist with id ${id} not found`);
     }
+
+    // Set artistId to null for all tracks that reference this artist
+    const allTracks = this.trackService.getAllTracks();
+    allTracks.forEach((track) => {
+      if (track.artistId === id) {
+        this.trackService.updateTrack(track.id, { artistId: null });
+      }
+    });
+
+    // Set artistId to null for all albums that reference this artist
+    const allAlbums = this.albumService.getAllAlbums();
+    allAlbums.forEach((album) => {
+      if (album.artistId === id) {
+        this.albumService.updateAlbum(album.id, { artistId: null });
+      }
+    });
 
     this.artistService.deleteArtist(id);
   }
